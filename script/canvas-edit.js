a945dbad32b77c48b35a3d144ca77b5dcc7e8618
K I N G <nelsonmichael4real@gmail.com>
2021-01-18 02:38:22 +0800

    wrote script that controls canvas size and color

--- script/canvas-edit.js
+++ script/canvas-edit.js
@@ -1,3 +1,4 @@
+// collating user inputs
 
 let resizeCanvas = document.getElementById("canvas");
 
@@ -7,22 +8,29 @@ let resizeCanvasHeight = document.getElementById("resize-height");
 
 let canvasColor = document.getElementById("color-picker1")
 
+let manualColorEntry = document.getElementById("color-value1");
 
+
+//the following functions mutates the canvas as the user inputs changes
 const resizeWidth = ()=>{
     resizeCanvas.style.width = `${resizeCanvasWidth.value}%`;
 };
 
 const resizeHeight = ()=>{
     resizeCanvas.style.height = `${resizeCanvasHeight.value}vh`;
-}
+};
 
 const bgColorChange = ()=>{
     resizeCanvas.style.backgroundColor = `${canvasColor.value}`;
     
-}
+};
 
+const manualBgColorChange = ()=>{
+    resizeCanvas.style.backgroundColor = `#${manualColorEntry.value}`;
+};
 
-
+//the following event listeners, listens for the user inputs and then calls its respective function
 resizeCanvasWidth.addEventListener('input', resizeWidth);
 resizeCanvasHeight.addEventListener('input', resizeHeight);
 canvasColor.addEventListener('input', bgColorChange);
+manualColorEntry.addEventListener('input', manualBgColorChange);
